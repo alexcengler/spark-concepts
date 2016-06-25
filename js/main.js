9385361127dcfbe17e4d9273637833e0d29ec683
alexcengler <alexcengler@gmail.com>
2016-06-25 15:30:08 -0400

    Visualizing starter data

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ svg1 = d3.select("#viz")
 	.append("g")
 	.attr("transform", "translate(" + padding.left + "," + padding.top + ")");
 
+
 nums1 = svg1.selectAll(".input_number")
 	.data(dat).enter()
 	.append("text")
@@ -32,33 +33,6 @@ nums1 = svg1.selectAll(".input_number")
 	.attr("x", 30)
 	.attr("y", function(d, i) { return ((i + 1) * 40) })
 	.text(function (d){ return d });
-
-
-multipy1 = svg1.selectAll(".multiply_number")
-	.data(dat).enter()
-	.append("text")
-	.attr("class", "multiply_number")
-	.attr("class", "number")
-	.attr("y", 40)
-	.attr("x", 700)
-	.text(function() { return "x " + multiply_input })
-	.transition()
-		.duration(1500)
-		.attr("x", 60)
-	.transition()
-		.duration(function(d,i) { return 2000 })
-		//.delay(1500)
-		.attr("y", function(d, i) { return ((i+1) * 40) });
-
-
-// results1 = svg1.selectAll(".results1_number")
-// 	.data(dat).enter()
-// 	.append("text")
-// 	.attr("class", "results1_number")
-// 	.attr("class", "number")
-// 	.attr("y", 40)
-// 		.attr("x", 700)
-// 		.text("x 2")
 
 
 
